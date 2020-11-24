42edb387e620f33d6b892474acfef955d2555693
Océ <oceane_000@hotmail.fr>
2020-11-24 15:07:27 +0100

    réparation

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,35 +8,34 @@ let points=0;
 let backgroundclean="#EEEEEE";
 let backgrounddark="#303030";
 
-function hidestart(id,idpassage) {
+function HideStart(id,idpassage) {
 	document.getElementById(id).style.display = 'none';
 	document.getElementById('aide').style.display = 'none';
 	document.getElementById(idpassage).style.display = 'flex'; 
 	  // indic none temporairement test//
-		document.getElementById('infos').style.display = 'flex'; 
+	document.getElementById('infos').style.display = 'flex'; 
 	niveau=[];
 }        
 
 
-function getRandomInt(max) {
+function GetRandomInt(max) {
   	return Math.floor(Math.random() * Math.floor(max));
 }
 
 function sleep(milliseconds) {
   	const date = Date.now();
   	let currentDate = null;
-  	do {
-    	currentDate = Date.now();
+  	do { currentDate = Date.now();
   	} while (currentDate - date < milliseconds);
 }
 
-function passageniveau(){
+function PassageNiveau(){
 
 	document.getElementById("sum").innerHTML=niveautotal;
 	document.getElementById("sum2").innerHTML=points;
 
 
-	let random =getRandomInt(4);
+	let random =GetRandomInt(4);
 	compteur=0;
 	etat=0;
 
@@ -59,74 +58,58 @@ function passageniveau(){
 		break;
 	}
 
-	console.log("Le nouveau tableau est : "+ niveau);
-
-// Affichage du pattern 
-	
-			
-	 switchcolor(niveau);
+	switchcolor(niveau);
 	etat=1;
 }
-// Recuperation des touches par le KeyEvent 
 
-
-
-
-
-function affKeyCode()
-{
-	//Si il a un tableau rempli
-	 if(etat==1){
-	if(event.keyCode){
-	 		if(event.keyCode==niveau[compteur]){
-
+function AffKeyCode(){
+	if(etat==1){
+		if(event.keyCode){
+		 	if(event.keyCode==niveau[compteur]){
 				if(niveau[compteur+1]!==undefined){
-					compteur++;
+					compteur++; 
 				} else {
 
-			 		setTimeout(function(){
-			 			document.getElementById('textes').style.display='none';
-			 			document.getElementById('alert').style.display = 'flex';
-			 			document.getElementById('infos').style.display = 'none'; 
-			 			document.getElementById('level1').style.display = 'none';
-							setTimeout(function(){
-								niveautotal=niveautotal+1;
-							console.log(niveautotal);
-							points=points+1;
-								document.getElementById('alert').style.display = 'none';
-								document.getElementById('infos').style.display = 'flex'; 
-			 				document.getElementById('level1').style.display = 'flex';
-			 						 			document.getElementById('textes').style.display='flex';
-
-							passageniveau()
-							},2500);
-							// document.getElementById(idpassage).style.display = 'flex'; 
-							
-
-						},100);
-
+				 	setTimeout(function(){
+					 	document.getElementById('textes').style.display='none';
+					 	document.getElementById('alert').style.display = 'flex';
+					 	document.getElementById('infos').style.display = 'none'; 
+					 	document.getElementById('level1').style.display = 'none';		
 						
-				}
-	 		} else {
-	 	
-	 			document.getElementById('textes').style.display='none';
-			 			document.getElementById('perdu').style.display = 'flex';
-			 			document.getElementById('infos').style.display = 'none'; 
-			 			document.getElementById('level1').style.display = 'none';
-	 			etat=0;
+						setTimeout(function(){
+							niveautotal=niveautotal+1;
+							points=points+1;
+							document.getElementById('alert').style.display = 'none';
+							document.getElementById('infos').style.display = 'flex'; 
+					 		document.getElementById('level1').style.display = 'flex';
+					 		document.getElementById('textes').style.display='flex';
 
-	 		}
-	 	}
-		
-	} else {
-	}
+							PassageNiveau()
+					
+						},2500);
+								
+
+					},100);
+			
+				}
+		 	} else {
+		 	
+		 		document.getElementById('textes').style.display='none';
+				document.getElementById('perdu').style.display = 'flex';
+				document.getElementById('infos').style.display = 'none'; 
+				document.getElementById('level1').style.display = 'none';
+		 		etat=0;
+
+		 	}
+		}	
+	} else {}
 	
 }
 
 
-   function Redirection(){
-        document.location.href="index.html";
-      }
+function Redirection(){
+    document.location.href="index.html";
+}
 
 function switchcolor(niveau){
 	console.log(niveau);
@@ -138,7 +121,7 @@ function switchcolor(niveau){
 
 
 	if(niveau[0] === undefined)
-		return
+		return;
 
 	switch(niveau[0]){
 
@@ -171,56 +154,45 @@ function switchcolor(niveau){
 
 	
 
-	setTimeout(function(){
-		document.getElementById(idel).style.backgroundColor = couleurarrv;
-	},500);
+setTimeout(function(){
+	document.getElementById(idel).style.backgroundColor = couleurarrv;
+},500);
 
 	
-	setTimeout(function(){ 
-		document.getElementById(idel).style.backgroundColor = couleurdep;
-		
-		switchcolor(niveau.slice(1,niveau.length)); 
-	},1000);
-
-
- 
+setTimeout(function(){ 
+	document.getElementById(idel).style.backgroundColor = couleurdep;
+	switchcolor(niveau.slice(1,niveau.length)); 
+},1000);
 
 	bloque = true
 	setTimeout(function(){
- bloque = false },niveau.length*1000); 
+ 		bloque = false },niveau.length*1000); 
 }
 
-document.body.addEventListener('keydown',affKeyCode);
+document.body.addEventListener('keydown',AffKeyCode);
 
 function Darkmode(nbCheck) {
+
     if(document.getElementById(nbCheck).checked == true){
-        		document.getElementById("body").style.backgroundColor = "#303030";
-        		document.getElementById("Titre").style.color="#B7A9A9";
-        		document.getElementById("sous-titre").style.color="#B7A9A9";
-        		document.getElementById("infos").style.color="#B7A9A9";
-        		document.getElementById("start").style.color='#302E2E';
-        		document.getElementById("start").style.backgroundColor= "#FFFFFF";
-        		document.getElementById("level1").style.backgroundColor="#575757";
-        		
-
-
-
-
+        document.getElementById("body").style.backgroundColor = "#303030";
+        document.getElementById("Titre").style.color="#B7A9A9";
+        document.getElementById("sous-titre").style.color="#B7A9A9";
+       	document.getElementById("infos").style.color="#B7A9A9";
+       	document.getElementById("start").style.color='#302E2E';
+       	document.getElementById("start").style.backgroundColor= "#FFFFFF";
+       	document.getElementById("level1").style.backgroundColor="#575757";
+       	document.getElementById("perdu").style.color="#B7A9A9";
+       	document.getElementById("alert").style.color="#B7A9A9";
     }
 
     else {
-    			document.getElementById("body").style.backgroundColor = "#EEEEEE";
-    			document.getElementById("Titre").style.color="black";
-    			document.getElementById("sous-titre").style.color="black";
-    			document.getElementById("infos").style.color="#212529";
-    			document.getElementById('aide').style.display = 'flex';
-
-
-
+   		document.getElementById("body").style.backgroundColor = "#EEEEEE";
+    	document.getElementById("Titre").style.color="black";
+    	document.getElementById("sous-titre").style.color="black";
+    	document.getElementById("infos").style.color="#212529";
     }
 }
 
-function changeClass() { 
+function ChangeClass() { 
     document.getElementById('textes').className = "row"; 
 }
-212529
